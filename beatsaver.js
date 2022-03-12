97c617df7d105123d6048c7ffb6e2f28b26faab9
joerkig <44418429+joerkig@users.noreply.github.com>
2022-03-12 06:30:36 +0100

    Update beatsaver.js

--- beatsaver.js
+++ beatsaver.js
@@ -1,7 +1,57 @@
 const WebSocket = require("ws"),
   XMLHttpRequest = require("xhr2"),
-  webhookURL = process.argv[2],
-  webhookURLBot = process.argv[3] ? process.argv[3] : process.argv[2];
+  webhookURL = process.argv[3],
+  webhookURLBot = process.argv[4] ? process.argv[4] : process.argv[3],
+  emojis = process.argv[2],
+  diffmojies = {
+    Easy: "<:easy:952058301611409408>",
+    Normal: "<:normal:952058301913366618>",
+    Hard: "<:hard:952058302093737994>",
+    Expert: "<:expert:952058301695295509>",
+    ExpertPlus: "<:expertplus:952058301586210846>",
+    Standard: "<:Standard:952058303398178886>",
+    StandardEasy: "<:standardeasy:952058302425100288>",
+    StandardNormal: "<:standardnormal:952058302496391189>",
+    StandardHard: "<:standardhard:952058302429278238>",
+    StandardExpert: "<:standardexpert:952058302408302612>",
+    StandardExpertPlus: "<:standardexpertplus:952058302429282354>",
+    OneSaber: "<:OneSaber:952058303507230770>",
+    OneSaberEasy: "<:onesabereasy:952058302609641492>",
+    OneSaberNormal: "<:onesabernormal:952058302320234496>",
+    OneSaberHard: "<:onesaberhard:952058302399938581>",
+    OneSaberExpert: "<:onesaberexpert:952058302525734942>",
+    OneSaberExpertPlus: "<:onesaberexpertplus:952058302383128596>",
+    NoArrows: "<:NoArrows:952058303100379196>",
+    NoArrowEasy: "<:noarrowseasy:952058302303436811>",
+    NoArrowsNormal: "<:noarrowsnormal:952058302550913044>",
+    NoArrowsHard: "<:noarrowshard:952058302383157258>",
+    NoArrowsExpert: "<:noarrowsexpert:952058302441873468>",
+    NoArrowsExpertPlus: "<:noarrowsexpertplus:952058302311854110>",
+    NinetyDegree: "<:90Degree:952058301569441822>",
+    NinetyDegreeEasy: "<:90degreeeasy:952058301145841694>",
+    NinetyDegreeNormal: "<:90degreenormal:952058301368107028>",
+    NinetyDegreeHard: "<:90degreehard:952058301267476491>",
+    NinetyDegreeExpert: "<:90degreeexpert:952058301162610718>",
+    NinetyDegreeExpertPlus: "<:90degreeexpertplus:952058301137424405>",
+    ThreeSixtyDegree: "<:360Degree:952058303213617172>",
+    ThreeSixtyDegreeEasy: "<:360degreeeasy:952058301523300382>",
+    ThreeSixtyDegreeNormal: "<:360degreenormal:952058301900804096>",
+    ThreeSixtyDegreeHard: "<:360degreehard:952058301389082644>",
+    ThreeSixtyDegreeExpert: "<:360degreeexpert:952058301569433610>",
+    ThreeSixtyDegreeExpertPlus: "<:360degreeexpertplus:952058301305221180>",
+    Lightshow: "<:Lightshow:952058303318462494>",
+    LightshowEasy: "<:lightshoweasy:952058301909205014>",
+    LightshowNormal: "<:lightshownormal:952058302030823426>",
+    LightshowHard: "<:lightshowhard:952058302282498048>",
+    LightshowExpert: "<:lightshowexpert:952058302223777852>",
+    LightshowExpertPlus: "<:lightshowexpertplus:952058302659969094>",
+    Lawless: "<:Lawless:952058303926665286>",
+    LawlessEasy: "<:lawlesseasy:952058302206984212>",
+    LawlessNormal: "<:lawlessnormal:952058301909184524>",
+    LawlessHard: "<:lawlesshard:952058302152450128>",
+    LawlessExpert: "<:lawlessexpert:952058302156664923>",
+    LawlessExpertPlus: "<:lawlessexpertplus:952058302282489916>",
+  };
 let ws = new WebSocket("wss://ws.beatsaver.com/maps");
 
 function connect() {
@@ -72,25 +122,41 @@ function connect() {
         };
       }
 
-      let diffs = {
-        characteristics: [],
-      };
-      obj.msg.versions[obj.msg.versions.length - 1].diffs.forEach((element) => {
-        if (!diffs[element.characteristic]) {
-          diffs[element.characteristic] = `${element.characteristic}: `;
-        }
-        if (!diffs.characteristics.includes(element.characteristic)) {
-          diffs.characteristics.push(element.characteristic);
-        }
-        diffs[element.characteristic] += `${element.difficulty.replace(
-          "Plus",
-          "+"
-        )}, `;
-      });
       let difficulties = "";
-      diffs.characteristics.forEach((element) => {
-        difficulties += `${diffs[element].slice(0, -2)}\n`;
-      });
+
+      if (emojis === "text") {
+        let diffs = {
+          characteristics: [],
+        };
+        obj.msg.versions[obj.msg.versions.length - 1].diffs.forEach(
+          (element) => {
+            if (!diffs[element.characteristic]) {
+              diffs[element.characteristic] = `${element.characteristic}: `;
+            }
+            if (!diffs.characteristics.includes(element.characteristic)) {
+              diffs.characteristics.push(element.characteristic);
+            }
+            diffs[element.characteristic] += `${element.difficulty.replace(
+              "Plus",
+              "+"
+            )}, `;
+          }
+        );
+        diffs.characteristics.forEach((element) => {
+          difficulties += `${diffs[element].slice(0, -2)}\n`;
+        });
+      } else if (emojis === "emojis") {
+        obj.msg.versions[obj.msg.versions.length - 1].diffs.forEach(
+          (element) => {
+            difficulties +=
+              diffmojies[
+                `${element.characteristic
+                  .replace("90", "Ninety")
+                  .replace("360", "ThreeSixty")}${element.difficulty}`
+              ];
+          }
+        );
+      }
 
       params.embeds[0].fields[params.embeds[0].fields.length] = {
         name: "Difficulties",
